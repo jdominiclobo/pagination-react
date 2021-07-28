d36c33a6a091dc96367fcc37136237e2ca04e8e6
joel dominic lobo <joe888dom@gmail.com>
2021-07-28 23:54:31 +0530

    pagination-react-hooks-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+import Posts from "./components/Posts";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="App">
+        <h1 className="text-primary mb-3">Fake Blog</h1>
+        <Posts posts={currentPosts} loading={loading} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Pagination.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Pagination = ({ postsPerPage, totalPosts }) => {
+  const pageNumbers = [];
+
+  return <div></div>;
+};
